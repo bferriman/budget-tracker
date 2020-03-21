eae8fc906296d0db32b550e9ccbdff617dd8dfc7
Ben F <56423163+bferriman@users.noreply.github.com>
2020-03-21 17:30:06 -0400

    moved indexedDB functionality to db.js file

--- public/index.js
+++ public/index.js
@@ -145,22 +145,6 @@ function sendTransaction(isAdding) {
   });
 }
 
-function saveRecord(trans) {
-  const request = window.indexedDB.open("transactionDB", dbVersion);
-
-  request.onupgradeneeded = ({ target }) => {
-    const db = target.result;
-    const objectStore = db.createObjectStore("transactionsList", { autoIncrement: true });
-  }
-
-  request.onsuccess = () => {
-    const db = request.result;
-    const transaction = db.transaction(["transactionsList"], "readwrite");
-    const objectStore = transaction.objectStore("transactionsList");
-    objectStore.add(trans);
-  }
-}
-
 document.querySelector("#add-btn").onclick = function() {
   sendTransaction(true);
 };
